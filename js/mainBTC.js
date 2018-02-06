b1cfb7e753f4274fdedcdd596125239543d42cbe
Stryge <jaunetetienne@gmail.com>
2018-02-07 00:07:36 +0100

    have to CORS request to do multiple get command from blockcypher API

--- js/mainBTC.js
+++ js/mainBTC.js
@@ -1,9 +1,22 @@
-$.get('https://api.blockcypher.com/v1/btc/main').then(function(json) {
+var invocation = new XMLHttpRequest();
+var url = 'https://api.blockcypher.com/v1/btc/main';
 
-var name = document.createElement("p");
-name.textContent = json.height;
+function callOtherDomain() {
+	if (invocation) {
+		invocation.open('GET', url, true);
 
-var starter = document.getElementsByClassName("starter template");
-console.log(starter[0]);
-starter[0].insertBefore(name, null);
+	}
+}
+
+$.get('https://api.blockcypher.com/v1/btc/main').then(function(lastBlock) {
+	var blockArray = new Array(5);
+
+	blockArray[0] = lastBlock;
+	for (var i = 1; i < 5; i++) {
+			blockArray[i] = $.get('https://api.blockcypher.com/v1/main/blocks/' + (lastBlock.height - i));
+	}
+	for (var i = 0; i < 5; i++) {
+		console.log(blockArray[i]);
+//		var hauteur = blockArray[i].height;
+	}
 });
