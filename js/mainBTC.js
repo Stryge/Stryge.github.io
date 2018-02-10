6a2ab714729ef2f4ce21f838de3c8dd0204b72ff
Etienne JAUNET <ejaunet@e2r4p3.42.fr>
2018-02-10 15:05:19 +0100

    add an XMLHttpRequest, let's see if it works

--- js/mainBTC.js
+++ js/mainBTC.js
@@ -1,3 +1,26 @@
+function getXMLHttpRequest() {
+	var xhr = null;
+
+	if (window.XMLHttpRequest || window.ActiveXObject) {
+		if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest();
+		}
+	} else {
+		alert ("XMLHttpRequest not supported");
+	}
+	return xhr;
+}
+
+var xhr = getXMLHttpRequest();
+xhr.open("GET", "blockcypher.com", true);
+xhr.send(null);
+
 $.get('https://api.blockcypher.com/v1/btc/main').then(function(lastBlock) {
 	var blockArray = new Array(5);
 
