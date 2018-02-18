6ac2a30772556cff835aad2746a68cca0908a2e6
Stryge <jaunetetienne@gmail.com>
2018-02-18 12:17:27 +0100

    Too many request to the API, fix it to do 3 request/sec max

--- js/mainBTC.js
+++ js/mainBTC.js
@@ -24,35 +24,40 @@ xhr.send(null);
 $.get('https://api.blockcypher.com/v1/btc/main').then(function(lastBlock) {
 	var blockArray = new Array(5);
 	var height = lastBlock.height
-	var i = 0;
+	var concatHeight = height;
 
-	while(i < 2) {
-			console.log(i + 1);
-			$.get('https://api.blockcypher.com/v1/btc/main/blocks/' + (height - i) + '?txstart=1&limit=1').then(function(block) {
-				console.log(i + 1);
-				var tr = document.createElement("tr");
-				var height = document.createElement("th");
-				var time = document.createElement("th");
-				var transactions = document.createElement("th");
-				var totalValue = document.createElement("th");
-				var relayedBy = document.createElement("th");
-
-				height.textContent = block.height;
-				time.textContent = block.time;
-				transactions.textContent = block.transactions;
-				totalValue.textContent = block.total;
-				relayedBy.textContent = block.relayed_by;
-				tr.appendChild(height); 
-				tr.appendChild(time); 
-				tr.appendChild(transactions); 
-				tr.appendChild(totalValue); 
-				tr.appendChild(relayedBy);
-				document.getElementByClassName("table").appendChild(tr);
-
-				console.log(tr);
-			});
-			i++;
+	for (var i = 1; i < 3; i++){
+		if (i != 3)
+			concatHeight += ";";
+		concatHeight += (height - i);
 	}
+
+	var url = 'https://api.blockcypher.com/v1/btc/main/blocks/509452;509451';
+	console.log(url);
+	$.get(url).then(function(block) {
+		console.log(block);
+		var tr = document.createElement("tr");
+		var height = document.createElement("th");
+		var time = document.createElement("th");
+		var transactions = document.createElement("th");
+		var totalValue = document.createElement("th");
+		var relayedBy = document.createElement("th");
+
+		height.textContent = block.height;
+		time.textContent = block.time;
+		transactions.textContent = block.transactions;
+		totalValue.textContent = block.total;
+		relayedBy.textContent = block.relayed_by;
+		tr.appendChild(height); 
+		tr.appendChild(time); 
+		tr.appendChild(transactions); 
+		tr.appendChild(totalValue); 
+		tr.appendChild(relayedBy);
+		var table = document.getElementsByClassName("table");
+		console.log(table[0]);
+
+		console.log(tr);
+	})
 });
 
 /*penser à augmenter la valeur des boucles for i < 2 à i < 5 */
